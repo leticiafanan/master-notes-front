dd20fd93b76afeefd31e41df646a8b645045ced5
Letícia <leticiafanan@gmail.com>
2024-12-19 16:41:25 -0300

    add new files, components

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,28 @@
 import './index.css';
 
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import {App} from './App.tsx'
+import { RouterProvider, createRouter } from '@tanstack/react-router';
+import { NuqsAdapter } from 'nuqs/adapters/react';
+import { StrictMode } from 'react';
+import ReactDOM from 'react-dom/client';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import { routeTree } from './routeTree.gen';
+
+const router = createRouter({ routeTree });
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof router;
+  }
+}
+
+// Render the app
+const rootElement = document.getElementById('root')!;
+if (!rootElement.innerHTML) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <StrictMode>
+      <NuqsAdapter>
+        <RouterProvider router={router} />
+      </NuqsAdapter>
+    </StrictMode>,
+  );
+}
